b9571547a42ba086e530440c31f28fea4ece7945
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-09-27 17:56:16 +0000

    steps 1-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css'
+import {data} from "./data.js"
 
 export default function App() {
   return (
-      <div id="display">
-      
+      <div id="drum-machine">
+        <div id="pads">
+          {data.map((pad)=>{
+        return <DrumPad name={pad.name} text={pad.text} key={pad.text} audio={pad.audio}/>;
+          })}
+        </div>
+        <div id="display" className='flex'>change text</div>
       </div>
   )
+}
+
+
+function DrumPad(props) {
+  return (<div id={props.name} className="drum-pad flex"
+    onClick={() => playSound(props.text, props.name)}>
+    <audio id={props.text} src={props.audio} className="clip"></audio>
+    {props.text}
+  </div>);
+}
+
+function playSound(id){
+  const sound = document.getElementById(id);
+  sound.play();
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('drum-machine')).render(
+ReactDOM.createRoot(document.getElementById('main')).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
