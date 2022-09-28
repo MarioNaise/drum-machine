c0283b0045181dcc8a2ad203d03bf164efad478c
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-09-28 17:11:31 +0000

    finished all steps

--- src/App.jsx
+++ src/App.jsx
@@ -6,50 +6,66 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      currentPad: {
-        text: ""
-      }
+      display: ""
     }
-  this.playSound = this.playSound.bind(this);
+    this.changeDisplay = this.changeDisplay.bind(this);
   }
 
-  componentDidMount(){
-    document.addEventListener("keydown", (e)=>{
-      const newPad = data.find(pad=>pad.text == e.key.toUpperCase());
-      this.setState({
-        currentPad: newPad
-      });
-      this.playSound(this.state.currentPad.text)
-    })
-  }
-  
-
-  playSound(id){
-    const sound = document.getElementById(id);
-    sound.play();
-    sound.currentTime = 0;
+  changeDisplay(name){
+    this.setState({
+      display: name
+    });
   }
   
   render (){
     return(
       <div id="drum-machine">
         <div id="pads">
-          {data.map((pad)=>{
-        return <DrumPad playSound={this.playSound} name={pad.name} text={pad.text} key={pad.text} audio={pad.audio}/>;
-          })}
+          {data.map((pad) => {return (<DrumPad changeDisplay={this.changeDisplay} key={pad.text} text={pad.text}/>)})}
         </div>
-        <div id="display" className='flex'>{this.state.currentPad.text}</div>
+        <div id="display" className='flex'>
+          {this.state.display || ""}
+        </div>
       </div>
     );
   }
     
 }
 
+class DrumPad extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pad: {}
+    }
+    this.handleKey = this.handleKey.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
 
-function DrumPad(props) {
-  return (<div id={props.name} className="drum-pad flex"
-    onClick={(e) => {console.log(e.target)}}>
-    <audio id={props.text} src={props.audio} className="clip"></audio>
-    {props.text}
-  </div>);
+  componentDidMount(){
+    document.addEventListener("keydown", this.handleKey)
+    const thisPad = data.find(pad => pad.text==this.props.text);
+    this.setState({
+      pad: thisPad
+    });
+  }
+
+  playSound(){
+    const sound = document.getElementById(this.state.pad.text);
+    sound.play();
+    sound.currentTime = 0;
+    this.props.changeDisplay(this.state.pad.name);
+  }
+
+  handleKey(e){
+    if(e.key.toUpperCase() === this.props.text){
+      this.playSound();
+    }
+  }
+
+  render(){
+    return (<div id={this.state.pad.name} className="drum-pad flex" onClick={this.playSound}>
+      <audio className="clip" id={this.state.pad.text} src={this.state.pad.audio}></audio>
+      {this.props.text}
+    </div>)};
 }
